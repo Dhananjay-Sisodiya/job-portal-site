d337eaa1c6637e25d12ddd3b9ad336d9e5e087bf
kshitijrat <kshitijratnawat@gmail.com>
2025-12-24 14:13:17 +0530

    update

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       const response = await axios.get(
         `${import.meta.env.VITE_BACKEND_URL}/api/v1/user/logout`,
         {
-          withCredentials: true,
+          withCredentials: false,
         }
       );
       toast.success(response.data.message);
